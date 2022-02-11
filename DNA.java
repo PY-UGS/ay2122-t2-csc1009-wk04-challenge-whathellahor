fd839950ff91dbdba2acc0c48220e444dca5a187
whathellahor <zihaoe1998@gmail.com>
2022-02-12 02:30:05 +0800

    Add files via upload

---
+++ DNA.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class DNA{
+    public static void main (String[] args){
+
+        Scanner userInput = new Scanner(System.in);
+        
+        System.out.println("Please enter your DNA sequence!");
+        String userString = (userInput.nextLine()).toUpperCase();
+        String targettedString = "GTC";
+        String infectionChance = "";
+        
+        int frequency = count(userString,targettedString); 
+
+        if(frequency>10){
+            infectionChance = "; infection chance is high";
+        }
+        System.out.println("sequence GTC appeared " + frequency + " times " + infectionChance);
+
+        userInput.close();
+    }
+    
+    public static int count(String str, String target) {
+        return (str.length() - str.replace(target, "").length())/target.length();
+    }
+}
+
